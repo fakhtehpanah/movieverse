d51f04e3565b744fa5b3ac27ac90db5b03f62bac
fakhtehpanah <fakhipanah@gmail.com>
2025-07-26 17:07:54 +0330

    fetch trending movies data from api

---
+++ movie-app/src/lib/tmdb.ts
@@ -0,0 +1,23 @@
+export interface Movie {
+    id: number;
+    title: string;
+    release_date?: string;
+    overview?: string;
+}
+
+interface TMDBResponse {
+  results: Movie[];
+}
+
+
+export async function fetchTrendingMovies(apiKey: string): Promise<Movie[]> {
+    const res = await fetch (`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`)
+    
+    if(!res.ok) {
+        throw new Error ('failed to fetch trending movies');
+    }
+    const data = (await res.json() as TMDBResponse);
+
+    return data.results || []
+    
+}  
